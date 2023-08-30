c302ae8b420bbce8e94c4937c39525c56d2008c5
nazario17 <hazarkorchak@gmail.com>
2023-08-30 15:05:49 +0300

    navigate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 
 const SharedLayout = lazy(() => import('../pages/SharedLayout'));
@@ -20,6 +20,7 @@ export const App = () => {
               <Route path="cast" element={<Cast />} />
               <Route path="reviews" element={<Reviews />} />
             </Route>
+            <Route path="*" element={<Navigate to="/" />} />
           </Route>
         </Routes>
       </Suspense>
